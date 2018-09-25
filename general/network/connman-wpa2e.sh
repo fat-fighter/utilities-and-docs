08eabc2c47b44ee0acefff96384912d911ffbb3a
Gurpreet Singh <g.singh6343@gmail.com>
2018-09-25 19:49:24 +0530

    Added script to connect WPA2-Enterprise networks

---
+++ general/network/connman-wpa2e.sh
@@ -0,0 +1,23 @@
+connmanctl scan wifi
+wifi=$( connmanctl services | fzf )
+
+ssid=$( echo $wifi | cut -d' ' -f2 )
+cnid=$( echo $wifi | cut -d' ' -f3 )
+
+echo -n "Username: "
+read uname
+echo -n "Password: "
+read -s upass
+echo ""
+
+sudo tee /var/lib/connman/${cnid}.config > /dev/null << END
+[service_peap]
+Type = wifi
+Name = $ssid
+EAP = peap
+Phase2 = MSCHAPV2
+Identity = $uname
+Passphrase = $upass
+END
+
+echo ""
